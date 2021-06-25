9fe3f06869b849e112a221390957f7d6a6883568
JoseH-Git <joseh.sierra@outlook.com>
2021-06-25 16:27:12 -0400

    table generation

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
 var tableData = data;
+// Assign the data from `data.js` to a descriptive variable
+// var people = data;
 
 // YOUR CODE HERE!
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(sight => sight.datetime === inputValue);
+
+  console.log(filteredData);
+};
+
+// BONUS: Refactor to use Arrow Functions!
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+data.forEach((weatherReport) => {
+    var row = tbody.append("tr");
+    Object.entries(weatherReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
